532a7dd2e94f0722ab7df72e466ac51834e8a6ae
HuangZhou <zhmail0830@gmail.com>
2024-07-02 16:15:32 +0800

    Add eslint prettier and init packages folder

---
+++ eslint.config.mjs
@@ -0,0 +1,39 @@
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import pluginReactConfig from 'eslint-plugin-react/configs/recommended.js';
+import { fixupConfigRules } from '@eslint/compat';
+
+export default [
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+      },
+    },
+  },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    files: ['**/*.jsx'],
+    languageOptions: { parserOptions: { ecmaFeatures: { jsx: true } } },
+  },
+  ...fixupConfigRules(pluginReactConfig),
+  {
+    rules: {
+      '@typescript-eslint/no-var-requires': 'off',
+    },
+  },
+  {
+    ignores: [
+      '**/node_modules/',
+      '**/coverage/',
+      '*.js',
+      'dist',
+      'packages/ui/*.d.ts',
+      'packages/utils/*.d.ts',
+      'packages/hooks/*.d.ts',
+      'scripts/**/*',
+    ],
+  },
+];
